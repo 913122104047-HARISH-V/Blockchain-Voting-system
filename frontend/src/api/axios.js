e5a0eb0cd3786411a447805cfe7317df70df4b6b
033_Alex-Pandian-M <alexpandian11205@gmail.com>
2026-02-14 15:20:13 +0530

    deleted all files

--- frontend/src/api/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:8000', // Change to your backend API prefix
-  withCredentials: true // If you need cookies/JWT
-});
-
-export default api;
